f68d2b7bf689ca2238ad72a261f1ba7e5b144d22
aksi <a.k.sattarov@gmail.com>
2019-03-09 01:31:54 +0300

    in function 'create_ad' added str 'area'

--- app/lib.py
+++ app/lib.py
@@ -1,10 +1,11 @@
-def create_ad(category,title,description,cost,contact):
+def create_ad(category,title,description,cost,contact,area):
     return {
         'category': category,
         'title': title,
         'description': description,
         'cost': cost,
-        'contact': contact
+        'contact': contact,
+        'area': area
     }
 
 def add_ad(container, ad):
