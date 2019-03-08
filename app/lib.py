f6f2ae9bcf57e24c92fd1bfa5d9f7112e79b00ea
aksi <a.k.sattarov@gmail.com>
2019-03-09 01:23:18 +0300

    Added created ad function 'create_ad'

---
+++ app/lib.py
@@ -0,0 +1,8 @@
+def create_ad(category,title,description,cost,contact):
+    return {
+        'category': category,
+        'title': title,
+        'description': description,
+        'cost': cost,
+        'contact': contact
+    }
