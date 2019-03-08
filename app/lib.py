0c4df3427aa4774a789307d555101950b5c642e4
aksi <a.k.sattarov@gmail.com>
2019-03-09 02:25:22 +0300

    added function search by price

--- app/lib.py
+++ app/lib.py
@@ -11,8 +11,10 @@ def create_ad(category,title,description,cost,contact,area):
 def add_ad(container, ad):
     container.append(ad)
 
-def search_by_districts(container, area):
+def search_by_districts(container, area, title):
     search_lowercased = area.strip().lower()
+    title_ad = title.strip().lower()
+
     result = []
     for ad in container:
             for i in ad['area']:
@@ -21,3 +23,13 @@ def search_by_districts(container, area):
                     continue
 
     return result
+
+def price_search(container, price):
+    result = []
+
+    for ad in container:
+        if price == ad['cost']:
+            result.append(ad)
+
+    return result
+
