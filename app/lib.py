56aed793e575babd640df605176f9818528ad979
aksi <a.k.sattarov@gmail.com>
2019-03-15 02:30:05 +0300

    deleted iter 'for ad in ads['area']'

--- app/lib.py
+++ app/lib.py
@@ -1,4 +1,10 @@
 def create_ad(category,title,description,cost,contact,area):
+
+    """
+    >>> create_ad('авто', 'ваз2101', '2011 года выпуска' ,100_000, '+7988123456', 'Новосавиновский')
+    {'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'}
+
+    """
     return {
         'category': category,
         'title': title,
@@ -9,18 +15,26 @@ def create_ad(category,title,description,cost,contact,area):
     }
 
 def add_ad(container, ad):
+
+    """
+    >>> add_ad([], {'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'})
+    """
+
     container.append(ad)
 
 def search_by_districts(container, area):
+
+    """
+    >>> search_by_districts([{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'},{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100_000, 'contact': '+7988123456', 'area': 'Вахитовский'}],'Новосавиновский')
+    [{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'}]
+    """
     search_lowercased = area.strip().lower()
     result = []
     for ads in container:
-        for ad in ads['area']:
-            if search_lowercased == ad.lower():
-                result.append(ads)
-                continue
-
+        if search_lowercased == ads['area'].lower():
+            result.append(ads)
     return result
+
 
 def search_by_price(container, price):
     result = []
--- app/main.py
+++ app/main.py
@@ -1,37 +1,50 @@
-from lib import create_ad, add_ad
+#
+#
+#
+# ads = []
+#
+# hello1 = create_ad (
+#     'авто',
+#     'ваз2101',
+#     '2011 года выпуска',
+#     100_000,
+#     '+7988123456',
+#     'Новосавиновский'
+# )
+# hello2 =  create_ad (
+#     'авто',
+#     'rr',
+#     '2018 года выпуска',
+#     20_000_000,
+#     '+7988123000',
+#     'Московский'
+# )
+#
+# add_ad(ads, hello1)
+# add_ad(ads, hello2)
+#
+# print(ads)
+#
+#
+# cost = 100_000
+# #search_lowercased = area.strip().lower()
+# result = []
+#
+# for ad in ads:
+#     if cost == ad['cost']:
+#        result.append(ad)
 
-
-ads = []
-
-hello1 = create_ad (
-    'авто',
-    'ваз2101',
-    '2011 года выпуска',
-    100_000,
-    '+7988123456',
-    'Новосавиновский'
-)
-hello2 =  create_ad (
-    'авто',
-    'rr',
-    '2018 года выпуска',
-    20_000_000,
-    '+7988123000',
-    'Московский'
-)
-
-add_ad(ads, hello1)
-add_ad(ads, hello2)
-
-#print(ads)
-
-
-cost = 100_000
-#search_lowercased = area.strip().lower()
+# print(result)
+# from lib import search_by_districts
+#
+# print(sea
+#
+# rch_by_districts([{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'}, {'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'}],'Новосавиновский'))
+container = [{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'Новосавиновский'},{'category': 'авто', 'title': 'ваз2101', 'description': '2011 года выпуска', 'cost': 100000, 'contact': '+7988123456', 'area': 'вахитовский'}]
+area = 'Новосавиновский'
+search_lowercased = area.strip().lower()
 result = []
-
-for ad in ads:
-    if cost == ad['cost']:
-       result.append(ad)
-
+for ad in container:
+        if search_lowercased == ad['area'].lower():
+            result.append(ad)
 print(result)
